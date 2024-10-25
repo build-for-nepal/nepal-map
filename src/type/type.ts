677223c500ee3ad9f5ba83c1b5e18aaa4570fff1
Rajan Midun Magar <rajan.midun1@gmail.com>
2024-10-26 01:16:52 +0545

    [Feature] Create basic Re-usable map component (#4)
    * feat(dependencies): install necessary dependencies
    
    * feat(cleanup): removed unnecessary files
    
    * feat(map-component): created a reusable map component to display the map
    
    * feat(type): seperate the type defination and updated the tsconfig to use json
    
    * feat(gejson): use the nepal geojson data
    
    * fix(extension): changed js to ts

---
+++ src/type/type.ts
@@ -0,0 +1,14 @@
+import { LatLngExpression } from 'leaflet';
+import { FeatureCollection } from 'geojson';
+
+export interface MapDataPoint {
+  position: LatLngExpression;
+  description: string;
+}
+
+export interface MapProps {
+  center: LatLngExpression;
+  zoom: number;
+  dataPoints: MapDataPoint[];
+  onFeatureClick?: (feature: any) => void; // Optional callback for feature clicks
+}
