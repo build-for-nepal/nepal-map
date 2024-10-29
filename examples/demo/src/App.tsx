83f771a33af59437b671d01396e94a7a6813ed6e
rznmgr630 <rajan.midun1@gmail.com>
2024-10-29 12:27:10 +0545

    feat(nepal-provinces): show the province of nepal

--- examples/demo/src/App.tsx
+++ examples/demo/src/App.tsx
@@ -1,6 +1,13 @@
 import { Map } from '../../../src/index';
-import Description from './component/Description';
+// import Description from './component/Description';
+import ProvinceDetail from './component/ProvinceDetail';
+
+import { provincesData } from './constants/province-data';
 function App() {
+  const dataPoints = provincesData.map(({ position, name, population }) => ({
+    position: position as unknown as [number, number],
+    description: <ProvinceDetail province={{ name, population }} />,
+  }));
   return (
     <div
       style={{
@@ -11,10 +18,11 @@ function App() {
       <Map
         center={[28.3949, 84.124]}
         zoom={7}
-        dataPoints={[
-          { position: [27.7, 85.3], description: <Description /> },
-          { position: [28.2, 83.9], description: 'Pokhara' },
-        ]}
+        // dataPoints={[
+        //   { position: [27.7, 85.3], description: <Description /> },
+        //   { position: [28.2, 83.9], description: 'Pokhara' },
+        // ]}
+        dataPoints={dataPoints}
       />
     </div>
   );
