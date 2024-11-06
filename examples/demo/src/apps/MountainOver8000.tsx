eeafd3578d21f703def86aca0f9d209ee9511e44
rznmgr630 <rajan.midun1@gmail.com>
2024-11-06 15:11:36 +0545

    feat(show-elevation): show the elevation detail in the title

--- examples/demo/src/apps/MountainOver8000.tsx
+++ examples/demo/src/apps/MountainOver8000.tsx
@@ -4,9 +4,11 @@ import { mountainsOver8000m } from '../constants/mountain-over.8000';
 
 const MountainOver8000 = () => {
   const dataPoints = mountainsOver8000m.map(
-    ({ coordinates, name, description }) => ({
+    ({ coordinates, name, description, elevation }) => ({
       position: coordinates as unknown as [number, number],
-      description: <Card property={{ name, description }} />,
+      description: (
+        <Card property={{ name: `${name} (${elevation})`, description }} />
+      ),
     }),
   );
 
