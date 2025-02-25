a76e5055b39e0524ff7bd517271de8216efa8d59
kritan <kritanshrestha560@gmail.com>
2025-02-25 12:31:55 +0545

    refactor(Card): remove unnecessary console log for property image

--- examples/demo/src/component/Card.tsx
+++ examples/demo/src/component/Card.tsx
@@ -9,7 +9,6 @@ type Props = {
 };
 
 const Card = ({ property }: Props) => {
-  console.log(property.image);
   return (
     <div style={styles.container}>
       <h3 style={styles.title}>{property.name}</h3>
